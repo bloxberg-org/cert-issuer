168a2716fda08e7080824c1fef51a1310440f2eb
xenia-melman <xenia.melman@gmail.com>
2020-01-16 16:26:08 +0100

    testing out

--- cert_issuer/secret_tester.py
+++ cert_issuer/secret_tester.py
@@ -1,10 +1,12 @@
 import requests
 
-from cert_issuer.models import SecretManager
 from cert_issuer.signer import FileSecretManager
 from cert_issuer.config import get_config
+from path_tools import get_pk_path
 
-secret = FileSecretManager("x", "/home/xenia/key.txt")
+
+secret_path = get_pk_path()
+secret = FileSecretManager("x", secret_path)
 print(secret.issuing_address)
 print(secret.path_to_secret)
 print(secret.signer)
