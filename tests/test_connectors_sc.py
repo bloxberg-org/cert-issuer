70f7f3634a4c5885f799fc9817dfe09a87941631
xenia-melman <xenia.melman@gmail.com>
2020-02-03 00:17:50 +0100

    anchor tests

---
+++ tests/test_connectors_sc.py
@@ -0,0 +1,18 @@
+import cert_issuer.blockchain_handlers.ethereum_sc.ens
+import unittest
+
+
+class TestSign(unittest.TestCase):
+    """
+    Tests if the anchor is being set correct
+    """
+    def test_ens_name():
+    self.app_config.ens_name
+
+
+    def test_contract_name:
+
+
+if __name__ == "__main__":
+    unittest.main()
+    print("Everything passed")
