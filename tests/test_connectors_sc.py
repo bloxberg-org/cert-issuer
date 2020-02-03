e20a4ecd06fc660013c8e20e96f7c34849aa06e5
xenia-melman <xenia.melman@gmail.com>
2020-02-03 14:39:06 +0100

    wip

--- tests/test_connectors_sc.py
+++ tests/test_connectors_sc.py
@@ -1,16 +1,20 @@
 import cert_issuer.blockchain_handlers.ethereum_sc.ens
 import unittest
+import json
+from cert_issuer import config
+app_config = config.get_config()
+with open("/home/xenia/Documents/PAS/cert-issuer/data/blockchain_certificates/4e7d75c5-281c-45de-93cc-3212b1349ee9.json", "r") as read_file
+data = json.loads(read_file)
 
 
 class TestSign(unittest.TestCase):
     """
     Tests if the anchor is being set correct
     """
-    def test_ens_name():
-    self.app_config.ens_name
+    def test_ens_name(self):
+        app_config.ens_name == data
 
-
-    def test_contract_name:
+    def test_contract_name(self):
 
 
 if __name__ == "__main__":
