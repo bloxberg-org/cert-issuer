a5b399852e6fa33f72d13cceff6cf99d8c0a5b6e
Andrei Ionita <andrei.ionita@fit.fraunhofer.de>
2020-11-06 16:55:35 +0100

    moving validation for non-web from pre_batch to finish_batch

--- cert_issuer/certificate_handlers.py
+++ cert_issuer/certificate_handlers.py
@@ -31,9 +31,10 @@ class CertificateV3Handler(CertificateHandler):
             certificate_json = json.load(unsigned_cert_file)
         return certificate_json
 
-    #def validate_certificate(self, certificate_metadata):
-    #    certificate_json = self._get_certificate_to_issue(certificate_metadata)
-    #    return validate_v3_alpha(certificate_json)
+    def validate_certificate(self, certificate_metadata):
+        with open(certificate_metadata.blockchain_cert_file_name, 'r') as signed_cert_file:
+            certificate_json = json.load(signed_cert_file)
+        return validate_v3_alpha(certificate_json)
 
     #def sign_certificate(self, signer, certificate_metadata):
         # TODO
@@ -55,7 +56,6 @@ class CertificateWebV3Handler(CertificateHandler):
         return certificate_json
 
     #def validate_certificate(self, certificate_metadata):
-    #    certificate_json = self._get_certificate_to_issue(certificate_metadata)
     #    return validate_v3_alpha(certificate_json)
 
     #def sign_certificate(self, signer, certificate_metadata):
@@ -72,6 +72,10 @@ class CertificateBatchWebHandler(BatchHandler):
             proof = next(proof_generator)
             self.proof.append(self.certificate_handler.add_proof(metadata, proof))
 
+        #for cert in self.certificates_to_issue:
+        #    self.certificate_handler.validate_certificate(cert)
+
+
     def get_certificate_generator(self):
         """
         Returns a generator (1-time iterator) of certificates in the batch
@@ -87,9 +91,6 @@ class CertificateBatchWebHandler(BatchHandler):
         Propagates exception on failure
         :return: byte array to put on the blockchain
         """
-
-        for cert in self.certificates_to_issue:
-            self.certificate_handler.validate_certificate(cert)
 
         self.merkle_tree.populate(self.get_certificate_generator())
         logging.info('here is the op_return_code data: %s', b2h(self.merkle_tree.get_blockchain_data()))
@@ -115,10 +116,6 @@ class CertificateBatchHandler(BatchHandler):
         :return: byte array to put on the blockchain
         """
 
-        # validate batch
-        for _, metadata in self.certificates_to_issue.items():
-            self.certificate_handler.validate_certificate(metadata)
-
         # sign batch
         with FinalizableSigner(self.secret_manager) as signer:
             for _, metadata in self.certificates_to_issue.items():
@@ -142,6 +139,9 @@ class CertificateBatchHandler(BatchHandler):
         for _, metadata in self.certificates_to_issue.items():
             proof = next(proof_generator)
             self.certificate_handler.add_proof(metadata, proof)
+        # validate batch
+        for _, metadata in self.certificates_to_issue.items():
+            self.certificate_handler.validate_certificate(metadata)
 
     def _process_directories(self, config):
         unsigned_certs_dir = config.unsigned_certificates_dir
